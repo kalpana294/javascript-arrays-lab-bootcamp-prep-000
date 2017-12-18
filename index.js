415f031e68ac481bf472f1a5893c9c3921a79236
Kalpana Swaminathan <kalpana294@gmail.com>
2017-12-18 20:55:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const app = "I don't do much."
 
 var kittens = ["Milo","Otis","Garfield"]
 function destructivelyAppendKitten(kittens){
-  kittens.shift("Ralph")
-  return kittens
+ return  kittens.shift("Ralph")
+  
 }
