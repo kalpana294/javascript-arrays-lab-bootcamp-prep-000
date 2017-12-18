09b7e4de8a585aa3628e686a16cf49dac6c9e0c6
Kalpana Swaminathan <kalpana294@gmail.com>
2017-12-18 20:53:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const app = "I don't do much."
 
 var kittens = ["Milo","Otis","Garfield"]
-Function destructivelyAppendKitten(kittens){
+function destructivelyAppendKitten(kittens){
   kittens.push("Ralph")
   return kittens
 }
